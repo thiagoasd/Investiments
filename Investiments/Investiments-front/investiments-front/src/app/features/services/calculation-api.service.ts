2e818663cc5ab9c9be204da7bd22405f3972cc50
Thiago Teixeira <thiago_teixeira4@yahoo.com.br>
2024-11-25 17:04:50 -0300

    Ajustes para deploy

--- Investiments/Investiments-front/investiments-front/src/app/features/services/calculation-api.service.ts
+++ Investiments/Investiments-front/investiments-front/src/app/features/services/calculation-api.service.ts
@@ -12,6 +12,6 @@ export class CalculationApiService {
   constructor(private http: HttpClient) { }
 
   getCdbCalculation(request: CdbCalculationRequest): Observable<CdbCalculation>{
-    return this.http.post<CdbCalculation>('Api/cdb/', request);
+    return this.http.post<CdbCalculation>('https://investmentsapp-fng8h9c9ezbqb8h8.brazilsouth-01.azurewebsites.net/cdb', request);
   }
 }
